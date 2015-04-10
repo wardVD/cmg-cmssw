a22d27f0f493c0095d58a681c7af76ae5760f6c3
schoef <robert.schoefbeck@cern.ch>
2015-04-10 12:08:57 +0200

    other stuff

--- CMGTools/TTHAnalysis/python/samples/samples_13TeV_private_heplx.py
+++ CMGTools/TTHAnalysis/python/samples/samples_13TeV_private_heplx.py
@@ -38,6 +38,18 @@ allComps=[]
 #  ) 
 #allComps.append(debugSample)
 
+aodDebugSample =   component = cfg.MCComponent(
+      dataset='',
+      name = 'test',
+      files = ['file:/data/schoef/local/WJetsToLNu_HT-100to200_Tune4C_13TeV-madgraph-tauola_PU20bx25_POSTLS170_V5-v1_AODSIM.root'],
+      xSection = 1.,
+      nGenEvents = 1,
+      triggers = [],
+      effCorrFactor = 1,
+  )
+ 
+allComps.append(aodDebugSample)
+
 DYJetsToLL_M50_PU20bx25           = createComponentFromDPM("DYJetsToLL_M50_PU20bx25",           "/DYJetsToLL_M-50_13TeV-madgraph-pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM", "/dpm/oeaw.ac.at/home/cms/store/user/schoef/metTuple3/DYJetsToLL_M-50_13TeV-madgraph-pythia8_Phys14DR-PU20bx25_PHYS14_25_V1-v1_AODSIM")
 DYJetsToLLHT100to200_M50_PU20bx25 = createComponentFromDPM("DYJetsToLLHT100to200_M50_PU20bx25", "/DYJetsToLL_M-50_HT-100to200_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM", "/dpm/oeaw.ac.at/home/cms/store/user/schoef/metTuple3/DYJetsToLL_M-50_HT-100to200_Tune4C_13TeV-madgraph-tauola_Phys14DR-PU20bx25_PHYS14_25_V1-v1_AODSIM")
 DYJetsToLLHT200to400_M50_PU20bx25 = createComponentFromDPM("DYJetsToLLHT200to400_M50_PU20bx25", "/DYJetsToLL_M-50_HT-200to400_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM", "/dpm/oeaw.ac.at/home/cms/store/user/schoef/metTuple3/DYJetsToLL_M-50_HT-200to400_Tune4C_13TeV-madgraph-tauola_Phys14DR-PU20bx25_PHYS14_25_V1-v1_AODSIM")
