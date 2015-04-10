762a70a34f3547f84a2da07dc4eb4231ced524b5
schoef <robert.schoefbeck@cern.ch>
2015-04-10 11:09:51 +0200

    cmg: fixing defaults in METAnalyzer susyCore

--- CMGTools/TTHAnalysis/python/analyzers/susyCore_modules_cff.py
+++ CMGTools/TTHAnalysis/python/analyzers/susyCore_modules_cff.py
@@ -328,7 +328,7 @@ metAna = cfg.Analyzer(
     candidates='packedPFCandidates',
     candidatesTypes='std::vector<pat::PackedCandidate>',
     dzMax = 0.1,
-    otherMETs=[],
+    collectionPostFix = "",
     )
 
 # Core Event Analyzer (computes basic quantities like HT, dilepton masses)
