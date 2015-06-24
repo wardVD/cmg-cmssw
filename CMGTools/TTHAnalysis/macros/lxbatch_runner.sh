9e4cbfb0de546d5855aa3d82ffb1622fa1b74ebd
Administrator <wardvandriessche@gmail.com>
2015-06-24 12:09:01 +0200

    change batch script

--- CMGTools/TTHAnalysis/macros/lxbatch_runner.sh
+++ CMGTools/TTHAnalysis/macros/lxbatch_runner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export SCRAM_ARCH=slc6_amd64_gcc481
-WORK=$1; shift
-SRC=$1; shift
+export WORK=$CMSSW_BASE/src/CMGTools/TTHAnalysis/cfg
+export SRC=$CMSSW_BASE/src 
 cd $SRC; 
 eval $(scramv1 runtime -sh);
 cd $WORK;
-exec $*
+heppy test run_susySinglelepton_test_cfg.py -f -N200 -p0 -o test=74X-MC -o sample=TT -o all=False -o single=True
