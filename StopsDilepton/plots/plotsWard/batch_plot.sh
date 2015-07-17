65681c74ea7fac4a0f3e2f6b7728b519ec4002a1
Administrator <wardvandriessche@gmail.com>
2015-07-17 14:12:35 +0200

    Deleting StopsDilepton

--- StopsDilepton/plots/plotsWard/batch_plot.sh
+++
@@ -1,7 +0,0 @@
-cd /afs/cern.ch/work/w/wvandrie/public/STOPS/CMSSW_7_2_3_patch1/src/
-eval `scramv1 runtime -sh`;
-scram b -j9
-mkdir -p /afs/cern.ch/user/w/wvandrie/eos;
-/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select -b fuse mount /afs/cern.ch/user/w/wvandrie/eos;
-cd /afs/cern.ch/work/w/wvandrie/public/STOPS/CMSSW_7_2_3_patch1/src/StopsDilepton/plots/plotsWard
-python plot.py
